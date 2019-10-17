aa7bac28f8e8a61b30ae7ddd96604735c044d43d
Ashley Cheng <ashwencheng@gmail.com>
2019-10-17 11:35:48 -0700

    Portfolio on-air
    home: horizontal slider + impact preview
    
    case studies: impact / epa / sbz / squek
    (all new layout)

--- js/index.js
+++ js/index.js
@@ -112,6 +112,7 @@ function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
 
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -126,19 +127,13 @@ function currentSlide(n) {
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
+    console.log(slides);
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
 
 $(document).ready(function () {
